60e38f6c2d5880d4bcb3142649c0d812c04d7d49
Brezker <brezker10@gmail.com>
2023-03-28 15:31:05 -0600

    Login mod

--- 8ids1react/src/components/LoginComponent.js
+++ 8ids1react/src/components/LoginComponent.js
@@ -1,80 +1,89 @@
-import React,{useState,useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
 import { Button } from 'primereact/button';
+import { Image } from 'primereact/image';
 
 const LoginComponent = () => {
 
   const endpoint = 'http://127.0.0.1:8000/api';
   const toast = useRef(null);
-  let navigate  = useNavigate();
+  let navigate = useNavigate();
 
-  const [datosLogin,setDatosLogin] = useState({
+  const [datosLogin, setDatosLogin] = useState({
     email: '',
     password: '',
   });
 
   const inputChange = (event) => {
-    
+
     setDatosLogin({
       ...datosLogin,
-      [event.target.name] : event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
-  function navigateTo(string)
-  {
-      navigate(string);
+  function navigateTo(string) {
+    navigate(string);
   }
-  
+
   const show = () => {
     toast.current.show({ severity: 'info', summary: 'Info', detail: 'Datos no válidos!!' });
   };
 
-  const fn_login = async (event)=> {
+  const fn_login = async (event) => {
     event.preventDefault();
 
     await axios.post(`${endpoint}/login`, datosLogin)
-    .then((response) => {
-      
-      console.log("Validando Acceso..")
-      console.log(response.data)
+      .then((response) => {
 
-      if(response.data.acceso === "Ok")
-      {
-        localStorage.setItem('token',"Ok")
-        navigateTo('/menu')
-      }
-      else
-      {
-        show();
-      }
+        console.log("Validando Acceso..")
+        console.log(response.data)
 
-    }).catch((error) => {
-      
-    })
+        if (response.data.acceso === "Ok") {
+          localStorage.setItem('token', "Ok")
+          navigateTo('/menu')
+        }
+        else {
+          show();
+        }
+
+      }).catch((error) => {
+
+      })
 
   }
 
   return (
-    <div>
+    <div class="contenedor">
+      <Image src="https://cdn-icons-png.flaticon.com/512/5087/5087579.png" alt="Image" width="250" />
       <div className="card flex justify-content-center">
-          <InputText name="email" value={datosLogin.email} onChange={(e) => inputChange(e)} />
+        <span className="p-input-icon-left">
+          <i className="pi pi-user" />
+          <InputText placeholder='usuario' name="email" value={datosLogin.email} onChange={(e) => inputChange(e)} />
+        </span>
+
       </div>
+      <br></br>
       <div className="card flex justify-content-center">
-          <Password name="password" 
-          value={datosLogin.password} 
-          feedback={false}
-          onChange={(e) => inputChange(e)} />
+        <span className="p-input-icon-left">
+          <i className="pi pi-key" />
+          
+          <InputText placeholder='Contraseña' name="password" type='password'
+            value={datosLogin.password}
+            feedback={false}
+            onChange={(e) => inputChange(e)} />
+        </span>
       </div>
+      <br></br>
       <div className="card flex justify-content-center">
-          <Button label="Accesar"  onClick={fn_login} />
+        <Button label="Accesar" onClick={fn_login} />
       </div>
     </div>
-      
-      
+
+
   )
 }
 
